d2ccc495da420abee99e4a0a8cb809ab24bb5542
AlgosStile <oleg.todor@mail.ru>
2024-05-31 12:28:44 +0300

    upd++++

--- src/test/java/by/peshkur/allure/pages/BasicPage.java
+++ src/test/java/by/peshkur/allure/pages/BasicPage.java
@@ -5,8 +5,8 @@ import static com.codeborne.selenide.Selenide.$;
 public class BasicPage {
 
     public void clickButton(String text) {
-        $("input[data-marker='login-form/login/input']").setValue("+79663758587");
-        $("input[data-marker='login-form/password/input']").setValue("dgdhdklrOL12");
+        $("input[data-marker='login-form/login/input']").setValue("your_logit_name");
+        $("input[data-marker='login-form/password/input']").setValue("password");
         $("button[data-marker='login-form/submit']").click();
 
     }
